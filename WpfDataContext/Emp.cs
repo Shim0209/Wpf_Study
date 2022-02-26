051ec3c5c655f1e51bb180a1d7ee204a4c0c2062
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2022-02-26 20:28:42 +0700

    Add New New Prj, DataContext 바인딩

---
+++ WpfDataContext/Emp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfDataContext
+{
+    class Emp
+    {
+        public string Ename { get; set; }
+        public string City { get; set; }
+    }
+}
