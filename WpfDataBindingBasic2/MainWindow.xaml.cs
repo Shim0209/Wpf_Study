a7f3f2b7a9cfd68ab0d80b20bf0444d11a1f18bb
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2022-02-26 17:18:46 +0700

    Update C#코드 바인딩, 타겟이 소스를 갱신

--- WpfDataBindingBasic2/MainWindow.xaml.cs
+++ WpfDataBindingBasic2/MainWindow.xaml.cs
@@ -20,6 +20,12 @@ using System.Windows.Shapes;
 /// C#코드에서 TextBox의 TextChanged 이벤트 핸들러를 이용하거나 XAML파일로 바인딩
 /// 
 /// 바인딩 자체는 언제나 타겟에 설정한다.
+/// 
+/// 바인딩모드 4가지
+/// 1. OneWay : Default 단방향 바인딩
+/// 2. TwoWay : Target의 변화도 Source에 반영된다.
+/// 3. OneTime : Target이 소스로 초기화 되자만 소스의변화가 계속 반영되지 않고 초기 한번만 반영
+/// 4. OneWayToSource : 타켓이 소스를 갱신하는 모양
 /// </summary>
 
 namespace WpfDataBindingBasic2
@@ -32,6 +38,14 @@ namespace WpfDataBindingBasic2
         public MainWindow()
         {
             InitializeComponent();
+
+            Binding bind = new Binding();
+            bind.Source = txt1;
+            bind.Path = new PropertyPath(TextBox.TextProperty);
+
+            // SetBinding의 첫 번째 인자는 DependencyProperty 타입이 되어야 한다.
+            // 그러므로 타겟의 속성은 의존속성의 지원을 받는 속성이 되야 한다.
+            label.SetBinding(Label.ContentProperty, bind);
         }
     }
 }
