baa96a66b2829652c273a4537f4e3bc1a955d141
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2022-03-04 23:56:01 +0700

    Add WpfListBox 실습

---
+++ WpfListBox/Model/Model.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfListBox.Model
+{
+    class Model : INotifyPropertyChanged
+    {
+        private string NAME_;
+        public string NAME
+        {
+            get
+            {
+                return this.NAME_;
+            }
+            set
+            {
+                this.NAME_ = value;
+                this.OnPropertyChanged("NAME");
+            }
+        }
+
+        private bool USE_;
+        public bool USE
+        {
+            get
+            {
+                return this.USE_;
+            }
+            set
+            {
+                this.USE_ = value;
+                this.OnPropertyChanged("USE");
+            }
+        }
+
+        private ObservableCollection<clsRCP> RCP_;
+        public ObservableCollection<clsRCP> RCP
+        {
+            get
+            {
+                return this.RCP_;
+            }
+            set
+            {
+                this.RCP_ = value;
+                this.OnPropertyChanged("RCP");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public Model()
+        {
+            this.RCP = new ObservableCollection<clsRCP>();
+            this.RCP.Add(new clsRCP() { NAME = "RCP1" });
+            this.RCP.Add(new clsRCP() { NAME = "RCP2" });
+            this.RCP.Add(new clsRCP() { NAME = "RCP3" });
+            this.RCP.Add(new clsRCP() { NAME = "RCP4" });
+        }
+    }
+}
---
+++ WpfListBox/Model/clsRCP.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfListBox.Model
+{
+    class clsRCP : INotifyPropertyChanged
+    {
+        private string NAME_;
+        public string NAME
+        {
+            get
+            {
+                return this.NAME_;
+            }
+            set
+            {
+                this.NAME_ = value;
+                this.OnPropertyChanged("NAME");
+            }
+        }
+
+        private bool USE_;
+        public bool USE
+        {
+            get
+            {
+                return this.USE_;
+            }
+            set
+            {
+                this.USE_ = value;
+                this.OnPropertyChanged("USE");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public clsRCP()
+        {
+
+        }
+    }
+}
