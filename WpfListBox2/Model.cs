fbbd47bf34860deffcb3761640b4a5967e51741d
Shim0209 <81503611+Shim0209@users.noreply.github.com>
2022-03-05 15:09:26 +0700

    Add ListBox2 실습 (MVVM의 기본구조 연습)

---
+++ WpfListBox2/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfListBox2
+{
+    class Model : INotifyPropertyChanged
+    {
+        private string NAME_;
+        public string NAME
+        {
+            get
+            {
+                return this.NAME_;
+            }
+            set
+            {
+                this.NAME_ = value;
+                OnPropertyChanged("NAME");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public Model() { }
+    }
+}
---
+++ WpfListBox2/ModelView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace WpfListBox2
+{
+    class ModelView : INotifyPropertyChanged
+    {
+        public RelayCommand AddCommand { get; set; }
+
+        private ObservableCollection<Model> ROOT_;
+
+        public ObservableCollection<Model> ROOT
+        {
+            get
+            {
+                return this.ROOT_;
+            }
+            set
+            {
+                this.ROOT_ = value;
+                this.OnPropertyChanged("RCP");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ModelView()
+        {
+            this.ROOT = new ObservableCollection<Model>();
+            this.AddCommand = new RelayCommand(AddExecuted, AddCanExecuted);
+        }
+
+        private bool AddCanExecuted(object obj)
+        {
+            return true;
+        }
+
+        private void AddExecuted(object obj)
+        {
+            this.ROOT.Add(new Model() { NAME = "TEST" });
+        }
+    }
+}
